96a73dd28c5254c3dddc333bfb3f2a65629b875c
mairarodrigues <maira.r.rodrigues@gmail.com>
2016-09-12 14:03:12 -0300

    Update cnvice_example.r

--- cnvice_example.r
+++ cnvice_example.r
@@ -1,10 +1,13 @@
 source("cnvice.r")
-Nj= c(78, 829, 2510, 756, 83, 9, 1)
+Nj = c(0,8,10,10,1)
 # Option 1
-executeCnvice(Nj,T,rept=10,document="output-report-withF.doc")
+executeCnvice(Nj,T,rept=10,document="output-report-withF")
 # Option 2
-executeCnvice(Nj,T,document="output-report-withF.doc")
+executeCnvice(Nj,T,document="output-report-withF")
+# Option 3
+executeCnvice(Nj,F,document="output-report-withoutF")
 
-# Option 3: For genotype estimation using trio information
-egmatrix<-executeCnvice(Nj,T,rept=10,document="output-report-withF.doc")
+# Option 4: For genotype estimation using trio information
+Nj= c(78, 829, 2510, 756, 83, 9, 1)
+egmatrix<-executeCnvice(Nj,T,rept=10,document="output-report-trio-withF")
 trio(4,3,3,egmatrix)
