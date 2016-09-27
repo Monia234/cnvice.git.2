eecf523d5d0f220b1581ce179fefa09ff04519c2
mairarodrigues <maira.r.rodrigues@gmail.com>
2016-09-27 20:16:38 -0300

    Add files via upload

--- cnvice_example.r
+++ cnvice_example.r
@@ -1,13 +1,20 @@
 source("cnvice.r")
-Nj = c(0,8,10,10,1)
-# Option 1
-executeCnvice(Nj,T,rept=10,document="output-report-withF")
-# Option 2
-executeCnvice(Nj,T,document="output-report-withF")
-# Option 3
-executeCnvice(Nj,F,document="output-report-withoutF")
 
-# Option 4: For genotype estimation using trio information
+Nj = c(0,8,10,10,1) 
+# Option 1
+executeCnvice(Nj,F,document="output-report-HW")
+# Option 2
+executeCnvice(Nj,T,document="output-report-F")	
+# Option 3
+executeCnvice(Nj,T,rept=10,document="output-report-repeat10")
+
+# Batch use
+CNVice<-executeCnvice(Nj,T)
+CNVice$allele_frequencies
+CNVice$genotype_frequencies2
+CNVice$genotype_frequencies
+
+# Option 3: For genotype estimation using trio information
 Nj= c(78, 829, 2510, 756, 83, 9, 1)
-egmatrix<-executeCnvice(Nj,T,rept=10,document="output-report-trio-withF")
-trio(4,3,3,egmatrix)
+CNVice<-executeCnvice(Nj,T)
+trio(4,3,3,CNVice$genotype_frequencies)
